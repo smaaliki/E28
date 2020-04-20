a7430c5ff4e6d2111061cdc081bb7b9219d29f9c
Samer Maaliki <smaaliki@hotmail.com>
2020-04-20 21:33:52 +0400

    P2: reading and displaying SPRs from a json file

--- p2/src/App.vue
+++ p2/src/App.vue
@@ -19,7 +19,7 @@ export default {
   name: 'App',
   data: function() {
         return {
-            links: ['home', 'dashboard', 'sprs']
+            links: ['home', 'sprs', 'dashboard' ]
         };
   },
   components: {
--- p2/src/components/pages/SPRsPage.vue
+++ p2/src/components/pages/SPRsPage.vue
@@ -1,9 +1,25 @@
 <template>
     <div>
         <p>Below is the list of the System Problem Reports (SPRs) that have been reported through the system.</p>
+        <div id='SPRs'>
+        <h2>SPRs</h2>
+        <div v-for='spr in sprs'
+            :key='spr.slug'
+           >
+           <p>{{ spr.title }} / {{ spr.status }}</p>
+           <p>{{ spr.description }}</p>
+           <p>{{ spr.priority }}</p>
+           </div>
+        <!--<show-product
+            v-for='spr in sprs'
+            :key='product.slug'
+            :spr='spr'
+            ></show-product>-->
+        </div>
     </div>
 </template>
 <script>
+import { sprs } from '@/seeds/sprs.js';
 
 export default {
     name:'',
@@ -11,6 +27,7 @@ export default {
     },
     data: function() {
         return{
+            sprs: sprs,
         };
     },
 
