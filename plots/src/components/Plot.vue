7a8205e3641d18e2ae9b3b5e346f69b809d95ac8
Samer Maaliki <smaaliki@hotmail.com>
2020-05-05 23:57:08 +0400

    plots: adding data and modifying the plot settings

--- plots/src/components/Plot.vue
+++ plots/src/components/Plot.vue
@@ -1,19 +1,63 @@
 <template>
   <div>
-    <p>Hello!</p>
-    <ul>
+    <h1>Let's Plot with D3.js and Vue!</h1>
+    <div id='data_div'>
+      <h2>Our data is</h2>
+      <table>
+        <thead id="dataTable">
+          <th>index</th>
+          <th>x Data</th>
+          <th>y Data</th>
+          <th>Actions</th>
+        </thead>
+        <tbody>
+          <tr v-for='(data,index) in myData' :key='index'>
+            <td>{{ index+1 }}</td>
+            <td>{{ data.x }}</td>
+            <td>{{ data.y }}</td>
+            <td>
+              <!--<img alt='edit data point' id='icon' src='@/assets/images/edit.png' v-on:click='editPoint(index)'/>-->
+              <img alt='delete data point' id='icon' src='@/assets/images/delete.png' v-on:click='deletePoint(index)'/>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+  <label for='xpoint'>x:</label>
+      <input type='text' v-model='tempPoint.x' id='xpoint' />
+      <br/>
+      
+      <label for='ypoint'>y:</label>
+      <input type='text' v-model='tempPoint.y' id='ypoint' />
+      <br/>
+      <button @click.prevent='addPoint'>Add Point</button>
+      
+    </div>
+    <!--<ul>
       <li>npm install d3</li>
-    </ul>
-    <p>Let's plot...</p>
-    <p>Our data is</p>
-    <ul>
-      <li v-for='(data,index) in xData' :key='data'>
-        ({{ data }}, {{yData[index]}})
-      </li>
-    </ul>
-    <p>Plot Width: {{ plotWidth }}</p>
-    <p>Plot Height: {{ plotHeight }}</p>
-    <div id="my_dataviz"></div>
+    </ul>-->
+  <div id='plot_div'>
+      <h2>Plot Settings</h2>
+    <div>
+      <label for='linecolor'>Line Color:</label>
+      <input type='color' v-model='settings.lineColor' id='linecolor' />
+      <br/>
+      
+      <label for='linewidth'>Line Width:</label>
+      <input type='text' v-model='settings.lineWidth' id='linewidth' />
+      <br/>
+      
+      <input type='submit' value='update' @click.prevent='redrawPlot' />
+    
+    </div>
+
+    <div>
+      <p>Plot Width: {{ plotWidth }}</p>
+      <p>Plot Height: {{ plotHeight }}</p>
+    </div>
+
+    <div id='my_dataviz'></div>
+  </div>
   </div>
 </template>
 
@@ -24,17 +68,20 @@ import * as d3 from 'd3'
 export default {
   name: 'Plot',
   data: function() {
-        return{
-      xData: [1,2,3,4,5],
-      yData: [2,4,6,8,10],
+    return{
       myData: [ { x: 1, y: 2 }, 
                 { x: 2, y: 4 },
-                { x: 3, y: 6},
+                { x: 3, y: 3},
                 { x: 4, y: 8},
-                { x: 5, y: 10}
+                { x: 5, y: 7}
                 ],
+      tempPoint: {x:1, y: 2},
       margin:  {top: 10, right: 30, bottom: 30, left: 60},
+      settings: {
+        lineColor: "#3333ba",
+        lineWidth: 2,
       }
+    }
   },
   computed: {
     plotWidth:  function () { 
@@ -44,63 +91,86 @@ export default {
       return 400 - this.margin.top - this.margin.bottom;
     }
   },
-mounted: function() {
-  console.log(this.myData);
-  this.myData.forEach(function(d) {
- d.x = +d.x;
- d.y = +d.y;
-});
-console.log(this.myData);
-  this.svg = d3.select("#my_dataviz")
-  .append("svg")
-    .attr("width", this.plotWidth + this.margin.left + this.margin.right)
-    .attr("height", this.plotHeight + this.margin.top + this.margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + this.margin.left + "," + this.margin.top + ")");
-/*          .append("rect")
-    .attr("width", "100%")
-    .attr("height", "100%")
-    .attr("fill", "pink");
-*/
-        // Add X axis --> it is a date format
-    var x = d3.scaleLinear()
-      .domain([0, 5])//d3.extent(data, function(d) { return d.date; }))
-      .range([ 0, this.plotWidth ]);
-    this.svg.append("g")
-      .attr("transform", "translate(0," + this.plotHeight + ")")
-      .call(d3.axisBottom(x));
+  mounted: function() {
+    this.myData.forEach(function(d) {
+      d.x = +d.x;
+      d.y = +d.y;
+    });
+          this.svg = d3.select("#my_dataviz")
+    .append("svg")
+      .attr("width", this.plotWidth + this.margin.left + this.margin.right)
+      .attr("height", this.plotHeight + this.margin.top + this.margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + this.margin.left + "," + this.margin.top + ")");
+    
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([0, 5])//d3.extent(data, function(d) { return d.date; }))
+    .range([ 0, this.plotWidth ]);
+
+  this.svg.append("g")
+    .attr("transform", "translate(0," + this.plotHeight + ")")
+    .call(d3.axisBottom(x));
       
-    // Add Y axis
-    console.log(d3.max(this.yData));
-    var y = d3.scaleLinear()
-      //.domain([0, d3.max(this.yData)])
-      .domain([0, 10])
-      .range([ this.plotHeight, 0 ]);
-      //console.log(y);
-    this.svg.append("g")
-      .call(d3.axisLeft(y));
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 10])
+    .range([ this.plotHeight, 0 ]);
 
-this.svg.append("path")
-      .datum(this.myData)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 1.5)
-      .attr("d", d3.line()
-        .x(function(d) { return x(d.x) })
-        .y(function(d) { return y(d.y) })
-        )
+  this.svg.append("g")
+    .call(d3.axisLeft(y));
 
 
-  }
+  },
+  methods: {
+    'addPoint': function addPoint() {
+        let x= this.tempPoint.x;
+        let y= this.tempPoint.y;
+        this.myData.push({x,y});
+      },
+      'deletePoint': function deletePoint(index) {
+      console.log(this.myData);
+      this.myData.splice(index,1);
+      console.log(this.myData);
+      this.redrawPlot();
+    },
+        'redrawPlot': function() {
+          console.log(this.settings.lineColor, this.settings.lineWidth);    
+          
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([0, 5])//d3.extent(data, function(d) { return d.date; }))
+    .range([ 0, this.plotWidth ]);
+
+  this.svg.append("g")
+    .attr("transform", "translate(0," + this.plotHeight + ")")
+    .call(d3.axisBottom(x));
+      
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 10])
+    .range([ this.plotHeight, 0 ]);
+
+  this.svg.append("g")
+    .call(d3.axisLeft(y));
+
+  this.svg.append("path")
+    .datum(this.myData)
+    .attr("fill", "none")
+    .attr("stroke", this.settings.lineColor)
+    .attr("stroke-width", this.settings.lineWidth)
+    .attr("d", d3.line()
+      .x(function(d) { return x(d.x) })
+      .y(function(d) { return y(d.y) })
+      );
+        }
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
 ul {
   list-style-type: none;
   padding: 0;
@@ -109,7 +179,54 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
-a {
-  color: #42b983;
+
+table {
+    font-family: 'Open Sans', sans-serif;
+    width: auto;
+    border-collapse: collapse;
+    border: 3px solid #44475C;
+    margin-left:auto;
+    margin-right:auto;
+}
+
+table th {
+    text-transform: uppercase;
+    text-align: left;
+    background: #44475C;
+    color: #FFF;
+    cursor: pointer;
+    padding: 8px;
+    min-width: 30px;
+}
+/*
+table th:hover {
+    background: #717699;
+}
+*/
+table td {
+  text-align: left;
+  padding: 8px;
+  border-right: 2px solid #7D82A8;
+}
+table td:last-child {
+  border-right: none;
+}
+table tbody tr:nth-child(2n) td {
+  background: #D4D8F9;
+}
+
+#icon {
+  width: 25px;
+  margin-right: 10px;
+}
+
+#data_div{
+  width: 50%;
+  float: left;
+}
+
+#plot_div{
+  width: 50%;
+  float: right;
 }
 </style>
